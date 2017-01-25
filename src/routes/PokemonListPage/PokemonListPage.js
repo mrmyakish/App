efaa3a479736e46f87a05b867c381edc5c9dfb6d
mrmyakish <xviterx@gmail.com>
2017-01-25 20:46:16 +0300

    Update PokemonListPage.js

--- src/routes/PokemonListPage/PokemonListPage.js
+++ src/routes/PokemonListPage/PokemonListPage.js
@@ -4,7 +4,7 @@ import Pokemons from 'components/Pokemons';
 export default () => (
   <div>
   <Pokemons />
-    Pokemons are not here  now <br/>
+    They come after 20 seconds <br/>
     <a href="/">Back to main page</a>
 
   </div>
